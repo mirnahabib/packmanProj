a212af1331bb04ddbec55d6c895c9c9d0f933cce
Mahmoud Eltelt <80259087+ElteltM@users.noreply.github.com>
2023-03-15 17:50:52 +0200

    added dummy data and query string for search

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-const express = require('express');
+const express = require('express')
+const {items} = require('./data')
 const app = express();
 
 app.get('/', (req, res) => {
   console.log('client request received')
   res.status(200).send('Home Page')
+})
+
+app.get('/api/v1/query', (req, res) => {
+  console.log(req.query)
+  const { search } = req.query
+  let sortedItems = [...items]
+
+  if (search) {
+    sortedItems = sortedItems.filter((item) => {
+      return item.name.includes(search)
+    })
+    if (sortedItems.length < 1) {
+      return res.status(200).send('no products matched your search');
+    }
+  }
+  res.status(200).json(sortedItems)
 })
 
 app.get('/about', (req, res) => {
