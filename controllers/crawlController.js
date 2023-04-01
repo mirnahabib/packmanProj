08fd6848413ca030b40fe78018523a2a17f9eda2
Mahmoud Eltelt <80259087+ElteltM@users.noreply.github.com>
2023-04-01 15:59:04 +0200

    Fixed: CrawlController returns results usin Sync method. TODO: add Categories and optimization.

--- controllers/crawlController.js
+++ controllers/crawlController.js
@@ -1,47 +1,45 @@
 const Item = require('../models/Item');
 const { StatusCodes } = require('http-status-codes');
 const path = require('path');
-const {spawn} = require("child_process");
+const {spawn, spawnSync} = require("child_process");
 
 const websites = new Map([
     ["amazon" ,  "./fetching/amazon.py"],
-    ["Jumia" , "./fetching/jumia.py"],
-    ["Noon" , "./fetching/noon.py"],
-    ["Select" , "./fetching/_select.py"],
-    ["Olx" , "./fetching/olx.py"],
-    ["Carrefour" , "./fetching/carrefour.py"],
-    ["Spinney" , "./fetching/spinney.py"],
-    ["Hyperone" , "./fetching/hyperone.py"],
-    ["Gourmet" , "./fetching/gourmet.py"],
-    ["Max" , "./fetching/max.py"],
-    ["H&M" , "./fetching/h&m.py"],
-    ["Bershka" , "./fetching/bershka.py"],
-    ["Zara" , "./fetching/zara.py"]
+    ["jumia" , "./fetching/jumia.py"],
+    ["noon" , "./fetching/noon.py"],
+    ["select" , "./fetching/_select.py"],
+    ["olx" , "./fetching/olx.py"],
+    ["carrefour" , "./fetching/carrefour.py"],
+    ["spinney" , "./fetching/spinney.py"],
+    ["hyperone" , "./fetching/hyperone.py"],
+    ["gourmet" , "./fetching/gourmet.py"],
+    ["max" , "./fetching/max.py"],
+    ["h&m" , "./fetching/h&m.py"],
+    ["bershka" , "./fetching/bershka.py"],
+    ["zara" , "./fetching/zara.py"]
 ])
 
 
 
 async function fetchWebsite(website, searchQuery) {
-    const script =  spawn("python" ,[path.join(__dirname, websites.get(website)), searchQuery])
-    console.log("fetch website has been called " + websites.get(website) + searchQuery);    
-    script.stdout.on('data' , (data) =>{
-        dataJson = data.toString()
-        console.log(dataJson);
-    })
-    script.on('close',(code) =>{
-        console.log(`ended ${website} ${code}`);
-    })
-    
+    const script =  spawnSync("python" ,[path.join(__dirname, websites.get(website)), searchQuery])
+    if(script.error){
+        console.log(error);
+    }else{
+        return script.output.toString();
+    }  
 }
 
 const crawl = async (req, res) => {
     const { search: search= 'playstation', cat: category = 'amazon' } = req.params;
-    const result = await fetchWebsite(category, search);
-    console.log(result);
-
-
-    
-    res.status(StatusCodes.OK).json({ result });
+    fetchWebsite(category, search)
+    .then((result) => {
+        console.log(result + ' method crawl');
+        res.status(StatusCodes.OK).json({ result });
+    })
+    .catch((error) =>{
+        console.log(error);
+    });  
 }
 
 module.exports = {crawl};
