d74757b931dcd794f8e39f72f9b7b2e7a284663d
Mahmoud Eltelt <80259087+ElteltM@users.noreply.github.com>
2023-04-15 23:28:09 +0200

    adding files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './Components/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom';
+function App() {
+  return (
+    <Router>
+      <Navbar/>
+    </Router>
+  );
+}
+
+export default App;
