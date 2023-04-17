3dd2d12189e1c073a85ca100896edb2c486d4a91
Mahmoud Eltelt <80259087+ElteltM@users.noreply.github.com>
2023-04-17 22:26:26 +0200

    added utils. Amazon SES integration required.

---
+++ utils/nodemailerConfig.js
@@ -0,0 +1,10 @@
+//this is for testing, and does not work. We should configure Amazon SES for email API.
+
+module.exports = {
+  host: 'smtp.ethereal.email',
+  port: 587,
+  auth: {
+    user: 'tommie.schamberger92@ethereal.email',
+    pass: '3FzkhF7Ut17qFdx3Qx',
+  },
+};
