2662a893f023d66e2f3145bac1efb6c2fd1018b3
mirnahabib <mirnahabib1222000@gmail.com>
2023-04-21 02:40:38 +0200

    added new responsive navbar

--- pacmanprjApp/pacmanprj/src/App.js
+++ pacmanprjApp/pacmanprj/src/App.js
@@ -13,6 +13,9 @@ import Announcer from './component/announcer';
 import Test from "./component/test";
 import Product from "./component/product";
 import Team from "./component/team";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "./component/imgs/logo.png";
+
 
 // const posts = [
 //   { id: '1', name: 'This first post is about React' },
@@ -57,8 +60,25 @@ function App() {
         <Route path='/signup' element={<Signup/>}/>
         <Route path='/test' element= {<Test/>}/> */}
       </Routes>
+      <Navbar expand="lg" className="Navbar" >
+          <Container>
+            <Navbar.Brand > <img src= {logo}/> </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbar-nav" />
+            <Navbar.Collapse id="navbar-nav">
+              <Nav>
+                <Nav.Link href="#" className="Font">Home</Nav.Link>
+                <Nav.Link href="#" className="Font">Login</Nav.Link>
+                <Nav.Link href="#" className="Font">Signup</Nav.Link>
+                <Nav.Link href="#" className="Font">Favorites</Nav.Link>
+                <Nav.Link href="#" className="Font">Team</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
 
       </div>
+
+      
    </Router>
   ); 
 }
