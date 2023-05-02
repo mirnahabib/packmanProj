150780d6e63c1e86036ea2411a2848bcc31dc804
mirnahabib <mirnahabib1222000@gmail.com>
2023-05-02 19:13:17 +0200

    added website icons

--- pacmanprjApp/pacmanprj/src/component/productcard.jsx
+++ pacmanprjApp/pacmanprj/src/component/productcard.jsx
@@ -7,6 +7,31 @@ import {
     OverlayTrigger,
     Tooltip,
   } from "react-bootstrap";
+import IconAmazon from "./imgs/amazon";
+import twoB from "./imgs/icons/twoB.png";
+import bershka from "./imgs/icons/bershka.png";
+import branto from "./imgs/icons/branto.png";
+import carrefour from "./imgs/icons/carrefour.png";
+import dpphone from "./imgs/icons/dpphone.jpg";
+import egygamer from "./imgs/icons/egygamer.png";
+import faces from "./imgs/icons/faces.png";
+import games2egypt from "./imgs/icons/games2egypt.jpg";
+import gameworld from "./imgs/icons/gameworld.jpg";
+import gourmet from "./imgs/icons/gourmet.jpg";
+import handm from "./imgs/icons/handm.png";
+import hubfurniture from "./imgs/icons/hubfurniture.png";
+import hyper1 from "./imgs/icons/hyper1.jpg";
+import ikea from "./imgs/icons/ikea.png";
+import jumia from "./imgs/icons/jumia.png";
+import lcwaikiki from "./imgs/icons/lcwaikiki.png";
+import max from "./imgs/icons/max.png";
+import noon from "./imgs/icons/noon.png";
+import olx from "./imgs/icons/olx.jpg";
+import opensooq from "./imgs/icons/opensooq.png";
+import select from "./imgs/icons/select.png";
+import shamy from "./imgs/icons/shamy.jpg";
+import spinneys from "./imgs/icons/spinneys.png";
+
 
 export default function Productcard(props) {
   return (
@@ -80,9 +105,35 @@ export default function Productcard(props) {
                       CHECK IT OUT
                     </Button>
                   </div>
-                  <small className="align-self-end text-end shop-name">
+                  {(props.product.Shop==="Amazon"? <IconAmazon/>:"")  }    
+                  {(props.product.Shop==="2B"? <img src={twoB} style={{width:100}} />:"")}
+                  {(props.product.Shop==="Bershka"? <img src={bershka}  />:"")}
+                  {(props.product.Shop==="Branto"? <img src={branto}  />:"")}
+                  {(props.product.Shop==="Carrefour"? <img src={carrefour} />:"")}
+                  {(props.product.Shop==="Dubai Phone"? <img src={dpphone} />:"")}
+                  {(props.product.Shop==="Egygamer"? <img src={egygamer} />:"")}
+                  {(props.product.Shop==="Faces"? <img src={faces} />:"")}
+                  {(props.product.Shop==="Games2egypt"? <img src={games2egypt} />:"")}
+                  {(props.product.Shop==="Gameworld"? <img src={gameworld} />:"")}
+                  {(props.product.Shop==="Gourmet"? <img src={gourmet} />:"")}
+                  {(props.product.Shop==="H&M"? <img src={handm} />:"")}
+                  {(props.product.Shop==="HubFurniture"? <img src={hubfurniture} />:"")}
+                  {(props.product.Shop==="Hyperone"? <img src={hyper1} />:"")}
+                  {(props.product.Shop==="Ikea"? <img src={ikea} />:"")}
+                  {(props.product.Shop==="Jumia"? <img src={jumia} />:"")}
+                  {(props.product.Shop==="LCWaikiki"? <img src={lcwaikiki} />:"")}
+                  {(props.product.Shop==="Max"? <img src={max} />:"")}
+                  {(props.product.Shop==="Noon"? <img src={noon} />:"")}
+                  {(props.product.Shop==="OLX"? <img src={olx} />:"")}
+                  {(props.product.Shop==="Opensooq"? <img src={opensooq} />:"")}
+                  {(props.product.Shop==="Select"? <img src={select} />:"")}
+                  {(props.product.Shop==="Shamy"? <img src={shamy} />:"")}
+                  {(props.product.Shop==="Spinneys"? <img src={spinneys} />:"")}
+                  
+                  
+                  {/* <small className="align-self-end text-end shop-name">
                     {props.product.Shop}
-                  </small>
+                  </small> */}
                 </div>
               </Card.Body>
             </Card>
