679a60243ddb77f171d67a8204d0d4ccefb765f5
omar-eldamrany <o.eldamrany@gmail.com>
2023-05-03 18:08:21 +0300

    better site logo handling

---
+++ pacmanprjApp/pacmanprj/src/component/logos.jsx
@@ -0,0 +1,57 @@
+import _2B from "./imgs/icons/2B.png";
+import bershka from "./imgs/icons/bershka.png";
+import brantu from "./imgs/icons/brantu.png";
+import carrefour from "./imgs/icons/carrefour.png";
+import egygamer from "./imgs/icons/egygamer.png";
+import faces from "./imgs/icons/faces.png";
+import games2egypt from "./imgs/icons/games2egypt.png";
+import gameworld from "./imgs/icons/gameworld.png";
+import gourmet from "./imgs/icons/Gourmet.png";
+import handm from "./imgs/icons/HnM.png";
+import hubfurniture from "./imgs/icons/hubfurnituree.png";
+import hyper1 from "./imgs/icons/Hyper1.png";
+import ikea from "./imgs/icons/ikea.png";
+import Jumia from "./imgs/icons/jumia.png";
+import Dubaiphone from "./imgs/icons/dubaiphone.png";
+import lcwaikiki from "./imgs/icons/lcwaikiki.png";
+import max from "./imgs/icons/max.png";
+import noon from "./imgs/icons/noon.png";
+import olx from "./imgs/icons/olx.png";
+import zara from "./imgs/icons/zara.png"
+import opensooq from "./imgs/icons/opensooq.png";
+import Select from "./imgs/icons/Select.png";
+import shamy from "./imgs/icons/elshamy.png";
+import spinneys from "./imgs/icons/spinneys.png";
+import Amazon from "./imgs/icons/Amazon.png";
+import townteam from "./imgs/icons/townteam.png"
+const Logos = new Map([
+  ["Amazon", Amazon],
+  ["Jumia", Jumia],
+  ["Noon", noon],
+  ["Dubai phone", Dubaiphone],
+  ["Select", Select],
+  ["2B", _2B],
+  ["Carrefour", carrefour],
+  ["Spinney", spinneys],
+  ["Hyperone", hyper1],
+  ["Gourmet", gourmet],
+  ["H&M" , handm],
+  ["Zara" , zara ],
+  ["Max",max],
+  ["Lc Waikiki" , lcwaikiki],
+  ["Bershka" , bershka],
+  ["Town Team", townteam],
+  ["Brantu" , brantu],
+  ["Faces" , faces ],
+  ["Ikea" , ikea],
+  ["Hub furniture", hubfurniture],
+  ["Games2Egypt" , games2egypt],
+  ["Shamy", shamy],
+  ["Egygamer" , egygamer],
+  ["Game World", gameworld],
+  ["OLX", olx],
+  ["Open Sooq", opensooq]
+
+]);
+
+export { Logos };
